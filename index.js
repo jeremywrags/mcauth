45f0f78f048064bab901dbe62af288ebd44bed09
Jeremy Ragsdale <jragsdale@jragsdale-ltm4.internal.salesforce.com>
2019-04-10 11:07:23 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var request = require("request");
+var bodyParser = require("body-parser");
+var Q = require('q');
+
+exports.getauth = function (version) {
+    var deferred = Q.defer();
+
+    var url = version = 'v1' ? 'https://auth.exacttargetapis.com/v1/requestToken' : 'https://' + process.env.tennant + '.auth.marketingcloudapis.com/v2/token'
+
+    var body = {
+        clientId: process.env.CLIENTID,
+        clientSecret: process.env.CLIENTSECRET
+    }
+
+    if(version == 'v2'){
+        
+        body.set('grant_type', 'client_credentials');
+    }
+    var options = {
+        method: 'POST',
+        url: url,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: body,
+        json: true
+    };
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            deferred.resolve(response.body.accessToken);                            
+        }         
+        else {
+            deferred.reject("Status Code: " + response.statusCode + "  Response Message: " + response.statusMessage);
+        }
+        deferred.promise.nodeify(callback);
+        return deferred.promise;
+    });       
+};
